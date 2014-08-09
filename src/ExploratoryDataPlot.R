df31a249b46804e12a5066710110783125e0894e
git <smendoza.barrera@gmail.com>
2014-08-09 09:38:09 -0500

    Change .gitignore
    To avoid big files of data

---
+++ src/ExploratoryDataPlot.R
@@ -0,0 +1,53 @@
+### ExploratoryDataPlot.R --- 
+## 
+## Filename: ExploratoryDataPlot.R
+## Description: 
+## Author: Sergio-Feliciano Mendoza-Barrera
+## Maintainer: 
+## Created: Sat Aug  9 09:14:48 2014 (-0500)
+## Version: 
+## Package-Requires: ()
+## Last-Updated: Sat Aug  9 09:17:02 2014 (-0500)
+##           By: Sergio-Feliciano Mendoza-Barrera
+##     Update #: 2
+## URL: 
+## Doc URL: 
+## Keywords: 
+## Compatibility: 
+## 
+######################################################################
+## 
+### Commentary: 
+## 
+## Exploratory Data Analysis
+## Project 1
+## 
+######################################################################
+## 
+### Change Log:
+## 
+## 
+######################################################################
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or (at
+## your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+## 
+######################################################################
+## 
+### Code:
+
+rm(list = ls())                         # Remove all workspace data
+
+
+######################################################################
+### ExploratoryDataPlot.R ends here
